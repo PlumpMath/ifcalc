2679a90c5b14b355338ec441e5dca55231a9e564
Kurt Mueller <mueller.128@osu.edu>
2015-07-12 14:26:50 -0400

    Added :bodyfat :percentage to db.

--- src/cljs/ifcalc/db.cljs
+++ src/cljs/ifcalc/db.cljs
@@ -1,8 +1,6 @@
 (ns ifcalc.db)
 
 (def default-db
-  {:name "re-frame"
-   :measurements {:weight {:amount 0
+  {:measurements {:weight {:amount 0
                            :unit "lb"}
-                  :height {:amount 0
-                           :unit "in"}}})
+                  :bodyfat {:percentage 0}}})
