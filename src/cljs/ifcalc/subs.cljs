95a9115b77709344ce59a3bd687c86880bf95258
Kurt Mueller <mueller.128@osu.edu>
2015-07-12 14:28:35 -0400

    Added bodyfat-change sub

--- src/cljs/ifcalc/subs.cljs
+++ src/cljs/ifcalc/subs.cljs
@@ -1,21 +1,27 @@
 (ns ifcalc.subs
     (:require-macros [reagent.ratom :refer [reaction]])
-    (:require [re-frame.core :as re-frame]))
+    (:require [re-frame.core :as re-frame :refer [register-sub]]))
 
-(re-frame/register-sub
+(register-sub
  :name
  (fn [db]
    (reaction (:name @db))))
 
-(re-frame/register-sub
+(register-sub
  :active-panel
  (fn [db _]
    (reaction (:active-panel @db))))
 
-(re-frame/register-sub
+(register-sub
  :weight-change
  (fn [db [_]]
    (let [weight-amount (reaction (get-in @db [:measurements :weight :amount]))
          weight-unit   (reaction (get-in @db [:measurements :weight :unit]))]
      (reaction {:amount @weight-amount
                 :unit   @weight-unit}))))
+
+(register-sub
+ :bodyfat-change
+ (fn [db [_]]
+   (let [bodyfat (reaction (get-in @db [:measurements :bodyfat]))]
+     (reaction @bodyfat))))
