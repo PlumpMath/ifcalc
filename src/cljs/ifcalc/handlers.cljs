08c7278096456b49446d75293681971a0ef1109d
Kurt Mueller <mueller.128@osu.edu>
2015-07-12 12:55:40 -0400

    Cerated :convert-weight handler

--- src/cljs/ifcalc/handlers.cljs
+++ src/cljs/ifcalc/handlers.cljs
@@ -1,18 +1,30 @@
 (ns ifcalc.handlers
-    (:require [re-frame.core :as re-frame]
-              [ifcalc.db :as db]))
+    (:require [re-frame.core :as re-frame :refer [register-handler]]
+              [ifcalc.db :as db]
+              [ifcalc.lib :as lib :refer [lb-to-kg
+                                          kg-to-lb]]))
 
-(re-frame/register-handler
+(register-handler
  :initialize-db
  (fn  [_ _]
    db/default-db))
 
-(re-frame/register-handler
+(register-handler
  :set-active-panel
  (fn [db [_ active-panel]]
    (assoc db :active-panel active-panel)))
 
-(re-frame/register-handler
+(register-handler
  :update-value
  (fn [db [_ value path]]
    (assoc-in db path value)))
+
+(register-handler
+  :convert-weight
+  (fn [db [_ amount unit]]
+    (let [updated-amount (if (= unit "lb")
+                           (lb-to-kg amount)
+                           (kg-to-lb amount))
+          updated-unit (if (= unit "kg") "lb" "kg")]
+      (assoc-in db [:measurements :weight] {:amount updated-amount
+                                            :unit updated-unit}))))
