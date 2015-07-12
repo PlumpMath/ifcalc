5cc15f701dc03996ced3009789ddeb9c3435db0b
Kurt Mueller <mueller.128@osu.edu>
2015-07-12 14:28:54 -0400

    Created bodyfat-input, bodyfat-pct-output, and lbm-output components

--- src/cljs/ifcalc/views.cljs
+++ src/cljs/ifcalc/views.cljs
@@ -1,11 +1,10 @@
 (ns ifcalc.views
     (:require-macros [reagent.ratom :refer [reaction]])
     (:require [re-frame.core :as re-frame :refer [subscribe dispatch]]
-              [reagent.core :as reagent :refer [atom]]))
+              [reagent.core :as reagent :refer [atom]]
+              [ifcalc.lib :as lib]))
 
 ;; --------------------
-
-
 
 (defn weight-input
   []
@@ -23,6 +22,18 @@
                 :value @unit
                 :on-click #(dispatch [:convert-weight @amount @unit])}]])))
 
+(defn bodyfat-input
+  []
+  (let [bodyfat    (subscribe [:bodyfat-change])
+        percentage (reaction (get @bodyfat :percentage))
+        pct        (atom @percentage)]
+    (fn []
+      [:div
+       [:input {:type "text"
+                :value @pct
+                :on-change #(do (reset! pct (-> % .-target .-value))
+                                (dispatch [:update-bodyfat-percentage @pct]))}]])))
+
 (defn weight-output
   []
   (let [weight (subscribe [:weight-change])
@@ -31,13 +42,37 @@
     (fn []
       [:h2 @amount @unit])))
 
+(defn bodyfat-pct-output
+  []
+  (let [bodyfat    (subscribe [:bodyfat-change])
+        percentage (reaction (get @bodyfat :percentage))]
+    (fn []
+      [:h2 @percentage])))
+
+(defn lbm-output
+  []
+  (let [weight  (subscribe [:weight-change])
+        bodyfat (subscribe [:bodyfat-change])
+        weight-amt  (reaction (get @weight :amount))
+        weight-unit (reaction (get @weight :unit))
+        bf-pct      (reaction (get @bodyfat :percentage))
+        lbm         (reaction (lib/lbm @weight-amt @bf-pct))]
+    (fn []
+      [:div [:h4 "Lean body mass"]
+            [:p @lbm]
+            [:h4 "BMR"]
+            [:p (lib/bmr @lbm @weight-unit)]])))
+
 
 (defn home-panel []
   (let [name (subscribe [:name])]
     (fn []
       [:div
        [weight-input]
+       [bodyfat-input]
        [weight-output]
+       [bodyfat-pct-output]
+       [lbm-output]
        [:div [:a {:href "#/about"} "Go to About Page"]]])))
 
 (defn about-panel []
