64c29ed42e130306cbc1afe58df73dc919bcce6c
Kurt Mueller <mueller.128@osu.edu>
2015-07-12 12:55:57 -0400

    Let’s make things simple first.

--- src/cljs/ifcalc/views.cljs
+++ src/cljs/ifcalc/views.cljs
@@ -1,41 +1,39 @@
 (ns ifcalc.views
     (:require-macros [reagent.ratom :refer [reaction]])
-    (:require [re-frame.core :as re-frame]))
+    (:require [re-frame.core :as re-frame :refer [subscribe dispatch]]
+              [reagent.core :as reagent :refer [atom]]))
 
 ;; --------------------
 
-(defn measurement-input [{:keys [amount unit path]}]
-  (let [amt (atom amount)]
-    (fn []
-      [:div
-       [:input {:type "text"
-                :value @amt
-                :on-change #(do (reset! amt (-> % .-target .-value))
-                                (re-frame/dispatch [:update-value @amt [:measurements path :amount]]))}]
-       [:input {:type "button"
-                :value unit}]])))
+
 
 (defn weight-input
   []
-  (let [weight (re-frame/subscribe [:weight-change])
+  (let [weight (subscribe [:weight-change])
         amount (reaction (get @weight :amount))
-        unit   (reaction (get @weight :unit))]
+        unit   (reaction (get @weight :unit))
+        amt    (atom @amount)]
     (fn []
-      [measurement-input {:amount @amount
-                          :unit @unit
-                          :path :weight}])))
+      [:div
+       [:input {:type "text"
+                :value @amount
+                :on-change #(do (reset! amt (-> % .-target .-value))
+                                (dispatch [:update-value @amt [:measurements :weight :amount]]))}]
+       [:input {:type "button"
+                :value @unit
+                :on-click #(dispatch [:convert-weight @amount @unit])}]])))
 
 (defn weight-output
   []
-  (let [weight (re-frame/subscribe [:weight-change])
+  (let [weight (subscribe [:weight-change])
         amount (reaction (get @weight :amount))
         unit   (reaction (get @weight :unit))]
     (fn []
-      [:h2 @amount])))
+      [:h2 @amount @unit])))
 
 
 (defn home-panel []
-  (let [name (re-frame/subscribe [:name])]
+  (let [name (subscribe [:name])]
     (fn []
       [:div
        [weight-input]
@@ -54,6 +52,6 @@
 (defmethod panels :default [] [:div])
 
 (defn main-panel []
-  (let [active-panel (re-frame/subscribe [:active-panel])]
+  (let [active-panel (subscribe [:active-panel])]
     (fn []
       (panels @active-panel))))
